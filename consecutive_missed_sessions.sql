f5d4ff371a57a5447b7e580a1090683535c2c41d
Neelima Pavuluri <neelu.pavuluri@outlook.com>
2024-07-28 23:10:23 -0700

    First Commit

---
+++ consecutive_missed_sessions.sql
@@ -0,0 +1,15 @@
+WITH consecutive_missed_sessions AS (
+ SELECT UserID, SessionDate,
+ CASE WHEN LAG(IsCompleted, 1, 1) OVER (PARTITION BY UserID ORDER BY SessionDate) = 0
+ THEN 1
+ ELSE 0
+ END AS missed_session,
+ SUM(CASE WHEN LAG(IsCompleted, 1, 1) OVER (PARTITION BY UserID ORDER BY SessionDate) = 0
+ THEN 1
+ ELSE 0
+ END) OVER (PARTITION BY UserID ORDER BY SessionDate) AS consecutive_missed
+ FROM WorkoutSessions
+)
+SELECT DISTINCT UserID
+FROM consecutive_missed_sessions
+WHERE consecutive_missed > 5 AND IsCompleted = 0;
